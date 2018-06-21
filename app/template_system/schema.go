c8c91d748355e70d2690f643b831ece2836582f7
Radosław Cymer <cymerrad@gmail.com>
2018-06-21 14:01:52 +0200

    nice one

--- app/template_system/schema.go
+++ app/template_system/schema.go
@@ -1,6 +1,6 @@
 package template_system
 
 var Schema map[string][]string = map[string][]string{
-	"action":       {"send_template"},
-	"template_tag": {"substitute_with_field", "insert_text"},
+	OnAction:      {DoActionSendTemplate},
+	OnTemplateTag: {DoSubstituteWithField, DoInsertText},
 }
