23afc0855e47e027da70265c599c3db091ef35a7
Radosław Cymer <cymerrad@gmail.com>
2018-06-21 17:00:43 +0200

    Slightly pointless abstraction

--- app/controllers/curl.go
+++ app/controllers/curl.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/go-gorp/gorp"
 	"github.com/revel/revel"
 	sq "gopkg.in/Masterminds/squirrel.v1"
 )
@@ -40,7 +39,7 @@ func (c Curl) Index() revel.Result {
 	if c.Validation.HasErrors() {
 		return c.Render()
 	}
-	settings, err := retrieveSettings(c.Txn)
+	settings, err := c.retrieveSettingsFromDB()
 	if err != nil {
 		c.Validation.Error("Error occurred: %s", err.Error())
 	}
@@ -54,6 +53,16 @@ func (c Curl) Index() revel.Result {
 	return c.Render()
 }
 
+func (c Curl) HasErrorsRedirect(val interface{}) (res revel.Result, ok bool) {
+	if c.Validation.HasErrors() {
+		// Store the validation errors in the flash context and redirect.
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(Curl.Index), true
+	}
+	return revel.ErrorResult{}, false
+}
+
 func (c Curl) Notify() revel.Result {
 	rawJSON := c.Params.Get("json")
 	input := make(map[string]interface{})
@@ -62,15 +71,11 @@ func (c Curl) Notify() revel.Result {
 		if strings.HasPrefix(err.Error(), `invalid character '\''`) {
 			c.Validation.Error("Use double quotes instead of single quotes.")
 		} else {
-			c.Validation.Error("Parsing returned this: %s", err.Error()) // FIXME REVEL HAS SOME PROBLEM WITH THIS LINE WITHOUT THE STRING
+			c.Validation.Error("Parsing returned this: %s", err.Error())
 		}
 	}
-
-	if c.Validation.HasErrors() {
-		// Store the validation errors in the flash context and redirect.
-		c.Validation.Keep()
-		c.FlashParams()
-		return c.Redirect(Curl.Index)
+	if res, ok := c.HasErrorsRedirect(Curl.Index); ok {
+		return res
 	}
 
 	event := models.EventParsed{}
@@ -81,20 +86,82 @@ func (c Curl) Notify() revel.Result {
 	prettiedUpInput, _ := json.MarshalIndent(input, "", "  ")
 
 	// creating temporary settings for testing purposes
-	// settings, err := retrieveSettings(c.Txn)
+	settings, err := c.retrieveSettingsFromSession()
+	if err != nil {
+		c.Validation.Error("Parsing returned this: %s", err.Error())
+	}
+	if res, ok := c.HasErrorsRedirect(Curl.Index); ok {
+		return res
+	}
+
+	templates, err := template_system.New(settings.OtherParsed, settings.Rules, settings.TemplatesRaw)
+	if err != nil {
+		c.Validation.Error("Error occurred: %s", err.Error())
+	}
+
+	c.ViewArgs["curl"] = CurlData{
+		Input:  string(prettiedUpInput),
+		Output: c.dryRun(event, templates),
+	}
+	if res, ok := c.HasErrorsRedirect(Curl.Index); ok {
+		return res
+	}
+
+	c.ViewArgs["settings"] = settings
+
+	// TODO frontend bugs
+	c.Log.Debugf("Settings: %#v", settings)
+
+	return c.RenderTemplate("Curl/Index.html")
+}
+
+func (c App) retrieveSettingsFromDB() (s SettingsData, err error) {
+	txn := c.Txn
+	var templatesRaw []models.NotifierTemplate
+	str, _, _ := sq.StatementBuilder.Select("*").From("NotifierTemplate").ToSql()
+	_, _ = txn.Select(&templatesRaw, str)
+
+	var rules []models.NotifierRule
+	str2, _, _ := sq.StatementBuilder.Select("*").From("NotifierRule").ToSql()
+	_, _ = txn.Select(&rules, str2)
+
+	settings := models.NotifierSettings{}
+	str3 := "SELECT * FROM NotifierSettings WHERE ID = ( SELECT MAX(ID) FROM NotifierSettings ) LIMIT 1"
+	err = txn.SelectOne(&settings, str3)
+	if err != nil {
+		return s, errors.New("no settings")
+	}
+
+	templatesParsed := make([]BodyParsed, len(templatesRaw))
+	for ind, raw := range templatesRaw {
+		templatesParsed[ind] = BodyParsed{raw.ID, string(raw.Body)}
+	}
+
+	schemaParsed, _ := json.Marshal(template_system.Schema)
+	settingsParsed, _ := settings.Unmarshall()
+
+	return SettingsData{
+		Templates:    templatesParsed,
+		Rules:        rules,
+		OtherParsed:  settingsParsed,
+		Schema:       string(schemaParsed),
+		Other:        string(settings.JSON),
+		TemplatesRaw: templatesRaw,
+	}, nil
+}
+
+func (c App) retrieveSettingsFromSession() (s SettingsData, err error) {
 	otherRaw := c.Params.Get("other")
 	other := models.NotifierSettingsParsed{}
 	err = json.NewDecoder(strings.NewReader(otherRaw)).Decode(&other)
 	if err != nil {
-		// TODO
-		// error validating
+		return s, err
 	}
 
 	cases := c.Params.Values["settings-cases"]
 	rules, err := template_system.ParseRulesFromValues(cases)
 	if err != nil {
-		// TODO
-		// error validating
+		return s, err
 	}
 	templatesRaw := []models.NotifierTemplate{}
 	key := templateKey{1}
@@ -128,63 +195,7 @@ func (c Curl) Notify() revel.Result {
 		Schema:       string(schemaParsed),
 	}
 
-	templates, err := template_system.New(settings.OtherParsed, settings.Rules, settings.TemplatesRaw)
-	if err != nil {
-		c.Validation.Error("Error occurred: %s", err.Error())
-	}
-
-	c.ViewArgs["curl"] = CurlData{
-		Input:  string(prettiedUpInput),
-		Output: c.dryRun(event, templates),
-	}
-
-	if c.Validation.HasErrors() {
-		// Store the validation errors in the flash context and redirect.
-		c.Validation.Keep()
-		c.FlashParams()
-		return c.Redirect(Curl.Index)
-	}
-
-	c.ViewArgs["settings"] = settings
-
-	// TODO frontend bugs
-	c.Log.Debugf("Settings: %#v", settings)
-
-	return c.RenderTemplate("Curl/Index.html")
-}
-
-func retrieveSettings(txn *gorp.Transaction) (s SettingsData, err error) {
-	var templatesRaw []models.NotifierTemplate
-	str, _, _ := sq.StatementBuilder.Select("*").From("NotifierTemplate").ToSql()
-	_, _ = txn.Select(&templatesRaw, str)
-
-	var rules []models.NotifierRule
-	str2, _, _ := sq.StatementBuilder.Select("*").From("NotifierRule").ToSql()
-	_, _ = txn.Select(&rules, str2)
-
-	settings := models.NotifierSettings{}
-	str3 := "SELECT * FROM NotifierSettings WHERE ID = ( SELECT MAX(ID) FROM NotifierSettings ) LIMIT 1"
-	err = txn.SelectOne(&settings, str3)
-	if err != nil {
-		return s, errors.New("no settings")
-	}
-
-	templatesParsed := make([]BodyParsed, len(templatesRaw))
-	for ind, raw := range templatesRaw {
-		templatesParsed[ind] = BodyParsed{raw.ID, string(raw.Body)}
-	}
-
-	schemaParsed, _ := json.Marshal(template_system.Schema)
-	settingsParsed, _ := settings.Unmarshall()
-
-	return SettingsData{
-		Templates:    templatesParsed,
-		Rules:        rules,
-		OtherParsed:  settingsParsed,
-		Schema:       string(schemaParsed),
-		Other:        string(settings.JSON),
-		TemplatesRaw: templatesRaw,
-	}, nil
+	return settings, err
 }
 
 func (c Curl) dryRun(event models.EventParsed, template *template_system.T) string {
